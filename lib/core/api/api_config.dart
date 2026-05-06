e28995093c39b374b52ab7ea9f5651c9a398a934
gaida-lisongo <inbtpkinshasa@gmail.com>
2026-05-06 06:48:55 +0200

    creating api"

--- lib/core/api/api_config.dart
+++ lib/core/api/api_config.dart
@@ -1,6 +1,5 @@
 class ApiConfig {
   static const String baseUrl = 'https://didactic-telegram-r46v47xqvqgwhx64g-8000.app.github.dev';
-  static const String apiPath = '/api';
 
-  static String get fullUrl => '$baseUrl$apiPath';
+  static String get fullUrl => baseUrl;
 }
