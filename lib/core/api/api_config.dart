73fccbd58776afebb94d7d9db6425d336fef7ec1
gaida-lisongo <inbtpkinshasa@gmail.com>
2026-05-06 06:27:05 +0200

    fix url server

--- lib/core/api/api_config.dart
+++ lib/core/api/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'http://172.20.10.14:3000'; // Assuming port 3000, common for backends.
+  static const String baseUrl = 'http://localhost:8000'; // Assuming port 3000, common for backends.
   static const String apiPath = '/api';
 
   static String get fullUrl => '$baseUrl$apiPath';
